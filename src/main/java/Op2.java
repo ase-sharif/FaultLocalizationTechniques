928a028f4d948b0982096ac28af67b172134c5cc
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-02-25 15:31:39 +0330

    initialize live test cases and calculate bad test coverage

--- src/main/java/Op2.java
+++ src/main/java/Op2.java
@@ -8,9 +8,13 @@
  */
 public class Op2 {
   private boolean[][] coverageMatrix; // coverage matrix -- [test][statement]
+  private boolean[] liveTestCases; // live test cases -- [test]
+  private boolean[] badCoverage; // bad coverage (no coverage information, usually due to a segmentation fault) -- [test]
+  private boolean isBadCoverageCalculated = false;
 
   private int numberOfTests; // number of test cases
   private int numberOfStatements; // number of statements
+
 
   /**
    * Constructor of Op2 class
@@ -20,6 +24,29 @@ public class Op2 {
     numberOfTests = coverageMatrix.length;
     numberOfStatements = coverageMatrix[0].length;
 
+    // initialize so that all test cases are live
+    liveTestCases = new boolean[numberOfTests];
+    for (int i = 0; i < liveTestCases.length; i++) {
+      liveTestCases[i] = true;
+    }
+
+  }
+
+  private void calculateBadTestCoverage() {
+    if (isBadCoverageCalculated)
+      return;
+    badCoverage = new boolean[numberOfTests];
+    for (int i = 0; i < numberOfTests; i++) {
+      badCoverage[i] = true;
+      for (int j = 0; j < numberOfStatements; j++) {
+        //if there is a statement covered for this test case this is not a bad test case
+        if (coverageMatrix[i][j]) {
+          badCoverage[i] = false;
+          break;
+        }
+      }
+      isBadCoverageCalculated = true;
+    }
   }
 
 }
