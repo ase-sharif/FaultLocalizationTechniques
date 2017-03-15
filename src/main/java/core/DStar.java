cf04b7e1b78526e1e68214b9df3b8624bf5790ff
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-15 13:57:18 +0330

    Create DStar class

---
+++ src/main/java/core/DStar.java
@@ -0,0 +1,24 @@
+package core;
+
+/**
+ * Calculates suspiciousness and confidence values according to the DStar fault localization
+ * technique.
+ * The usage mode is to create a coverage matrix that specifies which program
+ * elements are executed by which test cases. In terms of this implementation, each program element called
+ * a statement. coverage matrix is encoded as a two dimensional array where the first dimension is indexed by
+ * the test case number and the second dimension is indexed by the statement number.
+ */
+public class DStar extends FaultLocalizationTechnique {
+  /**
+   * Constructor of DStar class
+   *
+   * @param coverageMatrix
+   */
+  DStar(boolean[][] coverageMatrix) {
+    super(coverageMatrix);
+  }
+
+  void calculateSuspiciousnessAndConfidence() {
+
+  }
+}
