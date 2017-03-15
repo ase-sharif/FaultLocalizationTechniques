599128c67916d050384e863dbb0f4d15e1c71405
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2017-03-15 13:43:14 +0330

    Create Ochiai class

---
+++ src/main/java/core/Ochiai.java
@@ -0,0 +1,24 @@
+package core;
+
+/**
+ * Calculates suspiciousness and confidence values according to the Ochiai fault localization
+ * technique.
+ * The usage mode is to create a coverage matrix that specifies which program
+ * elements are executed by which test cases. In terms of this implementation, each program element called
+ * a statement. coverage matrix is encoded as a two dimensional array where the first dimension is indexed by
+ * the test case number and the second dimension is indexed by the statement number.
+ */
+public class Ochiai extends FaultLocalizationTechnique {
+  /**
+   * Constructor of Ochiai class
+   *
+   * @param coverageMatrix
+   */
+  Ochiai(boolean[][] coverageMatrix) {
+    super(coverageMatrix);
+  }
+
+  void calculateSuspiciousnessAndConfidence() {
+
+  }
+}
